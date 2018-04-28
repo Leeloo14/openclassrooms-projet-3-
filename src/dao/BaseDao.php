ce9b505cca0038b6a5268b706a839353eb8e565e
remimichel <remi.michel38@gmail.com>
2018-04-28 12:48:19 +0200

    update config

--- src/dao/BaseDao.php
+++ src/dao/BaseDao.php
@@ -9,11 +9,11 @@ class BaseDao
     protected function dbConnect()
     {
         $dbInfo = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        //$dbInfo = array("host" => "eu-cdbr-west-02.cleardb.net", "path" => "heroku_a19cc1652a3e5c5", "user" => "b2cecd77104393", "pass" => "19ade141");
-        var_dump($dbInfo);
-        //var_dump('mysql:host='. $dbInfo["host"] .';dbname='. $dbInfo["path"] .';charset=utf8');
-        $db = new \PDO('mysql:host='. $dbInfo["host"] .';dbname='. $dbInfo["path"] .';charset=utf8', $dbInfo["user"], $dbInfo["pass"]);
-        return $db;
+        $server = $dbInfo["host"];
+        $username = $dbInfo["user"];
+        $password = $dbInfo["pass"];
+        $db = substr($dbInfo["path"], 1);
+        return new \PDO('mysql:host='. $server .';dbname='. $db .';charset=utf8', $username, $password);
     }
 
     protected function Q($data){
