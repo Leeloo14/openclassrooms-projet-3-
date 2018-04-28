5e2068b4373360723f92960ad8a03fed6cf63aa3
remimichel <remi.michel38@gmail.com>
2018-04-28 12:31:05 +0200

    update config

--- src/dao/BaseDao.php
+++ src/dao/BaseDao.php
@@ -9,7 +9,7 @@ class BaseDao
     protected function dbConnect()
     {
         $dbInfo = getenv("CLEARDB_DATABASE_URL");
-        $db = new \PDO('mysql:host='. $dbInfo["host"] .';dbname=blog;charset=utf8', $dbInfo["user"], $dbInfo["pass"]);
+        $db = new \PDO('mysql:host='. $dbInfo["host"] .';dbname='. $dbInfo["path"] .';charset=utf8', $dbInfo["user"], $dbInfo["pass"]);
         return $db;
     }
 
