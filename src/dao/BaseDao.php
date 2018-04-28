17c31e0b33e502df17c49caa9638638760556a7d
remimichel <remi.michel38@gmail.com>
2018-04-28 12:32:33 +0200

    view config

--- src/dao/BaseDao.php
+++ src/dao/BaseDao.php
@@ -8,7 +8,10 @@ class BaseDao
 {
     protected function dbConnect()
     {
-        $dbInfo = getenv("CLEARDB_DATABASE_URL");
+        $dbInfo = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        //$dbInfo = array("host" => "eu-cdbr-west-02.cleardb.net", "path" => "heroku_a19cc1652a3e5c5", "user" => "b2cecd77104393", "pass" => "19ade141");
+        var_dump($dbInfo);
+        //var_dump('mysql:host='. $dbInfo["host"] .';dbname='. $dbInfo["path"] .';charset=utf8');
         $db = new \PDO('mysql:host='. $dbInfo["host"] .';dbname='. $dbInfo["path"] .';charset=utf8', $dbInfo["user"], $dbInfo["pass"]);
         return $db;
     }
